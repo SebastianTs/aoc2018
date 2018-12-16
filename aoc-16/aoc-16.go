4fe20dc9b92ffe7bb5e2cb9d5c6deaf8b55d6552
s.tschiedel <s.tschiedel@gmail.com>
2018-12-16 23:20:05 +0100

    added part2 to the puzzle made variable names more meaningful

--- aoc-16/aoc-16.go
+++ aoc-16/aoc-16.go
@@ -6,32 +6,70 @@ import (
 	"os"
 )
 
-
+const (
+	numberOfOperations = 16
+	splitLine = 3129
+)
 
 func main() {
-	vs, err := readInput("./input.txt")
+	vs, ops, err := readInput("./input.txt")
 	if err != nil {
 		panic(err)
 	}
 	fns := []opFunc{addr, addi, mulr, muli, banr, bani, borr, bori, setr, seti, gtir, gtri, gtrr, eqir, eqri, eqrr}
+	mapping := make(map[int]map[int]struct{})
+	// setup
 	gCount := 0
+	for i:=0; i < numberOfOperations; i++{
+		mapping[i] = make(map[int]struct{})
+	}
+
+	// looking for functions
 	for _, v := range vs{
-		if check(v,fns) > 2 {
+		index := matchRegistersToOperations(v,fns)
+		if len(index) >= 3 {
 			gCount++
 		}
+		for j:=0; j < len(index); j++{
+			mapping[v.op.opCode][index[j]] = struct{}{}
+		}
 	}
-	fmt.Println(gCount)
+	fmt.Println( gCount, "behave like three or more opcodes")
+
+	ready := false
+	opCodes := make(map[int]opFunc)
+	for !ready{
+		for k,v := range mapping{
+			if len(v) == 1 {
+				for code := range v{
+					opCodes[k] = fns[code]
+					for _, candidates := range mapping{
+						delete(candidates,code)
+					}
+				}
+				if len(opCodes) == numberOfOperations {
+					ready = true
+				}
+			}
+		}
+	}
+	g := register{}
+	for _, op := range ops{
+		g = opCodes[op.opCode](op.A,op.B,op.O,g)
+	}
+
+	fmt.Println(g[0], "is contained in register 0 after executing the test program")
 
 }
 
-func check(n note,fn []opFunc) (count int){
-	for _, f := range fn{
-		cur := f(n.op.inputA, n.op.inputB, n.op.output, n.before)
+func matchRegistersToOperations(n sample,fn []opFunc) (index []int){
+	for i, f := range fn{
+		cur := f(n.op.A, n.op.B, n.op.O, n.before)
 		if cur == n.after{
-				count++
+				index = append(index,i)
 		}
 	}
-	return count
+	return index
 }
 
 type opFunc func (a,b,c int, r register) register
@@ -141,7 +179,7 @@ func eqrr(a,b,c int, r register) register{
 	return r
 }
 
-type note struct {
+type sample struct {
 	before register
 	op operation
 	after register
@@ -149,43 +187,51 @@ type note struct {
 
 type register [4]int
 type operation struct{
-	instruction int
-	inputA int
-	inputB int
-	output int
+	opCode int
+	A      int
+	B      int
+	O      int
 }
-func readInput(path string) (vs []note, err error) {
-	vs = make([]note,0)
 
+func readInput(path string) (vs []sample, testprogram []operation, err error) {
+	vs = make([]sample, 0)
+	testprogram = make([]operation, 0)
 	f, err := os.Open(path)
 	defer f.Close()
 	if err != nil {
-		return vs,  err
+		return vs, testprogram, err
 	}
 	scanner := bufio.NewScanner(f)
-	var aBefore,bBefore,cBefore,dBefore,aAfter,bAfter,cAfter, dAfter,ins, aInput,bInput,output int
+	var aBefore, bBefore, cBefore, dBefore, aAfter, bAfter, cAfter, dAfter, ins, aInput, bInput, output int
 	count := 0
 	for scanner.Scan() {
-		 switch count%4{
-		 case 0:
-			_, err = fmt.Sscanf(scanner.Text(), "Before: [%d, %d, %d, %d]", &aBefore, &bBefore, &cBefore, &dBefore)
-		 case 1:
-			 _, err = fmt.Sscanf(scanner.Text(), "%d %d %d %d", &ins, &aInput, &bInput, &output)
-		 case 2:
-			 _, err = fmt.Sscanf(scanner.Text(), "After: [%d, %d, %d, %d]", &aAfter, &bAfter, &cAfter, &dAfter)
-		 case 3:
-			 if err != nil {
-				 return vs, err
-			 }
-			 before := register{aBefore,bBefore,cBefore,dBefore}
-			 after := register{aAfter,bAfter,cAfter,dAfter}
-			 instruction := operation{ins, aInput, bInput, output}
-			 vs = append(vs,note{before,instruction,after})
+		if count > splitLine {
+			_, err = fmt.Sscanf(scanner.Text(), "%d %d %d %d", &ins, &aInput, &bInput, &output)
+			if err != nil {
+				return vs, testprogram, err
+			}
+			testprogram = append(testprogram, operation{ins, aInput, bInput, output})
+
+		} else {
+			switch count % 4 {
+			case 0:
+				_, err = fmt.Sscanf(scanner.Text(), "Before: [%d, %d, %d, %d]", &aBefore, &bBefore, &cBefore, &dBefore)
+			case 1:
+				_, err = fmt.Sscanf(scanner.Text(), "%d %d %d %d", &ins, &aInput, &bInput, &output)
+			case 2:
+				_, err = fmt.Sscanf(scanner.Text(), "After: [%d, %d, %d, %d]", &aAfter, &bAfter, &cAfter, &dAfter)
+			case 3:
+				if err != nil {
+					return vs, testprogram, err
+				}
+				before := register{aBefore, bBefore, cBefore, dBefore}
+				after := register{aAfter, bAfter, cAfter, dAfter}
+				instruction := operation{ins, aInput, bInput, output}
+				vs = append(vs, sample{before, instruction, after})
+			}
 		}
-		 count++
-		 if count == 3129{
-		 	break
-		 }
+		count++
+
 	}
-	return vs, nil
+	return vs, testprogram, nil
 }
